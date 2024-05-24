30613ea5ab768b377bc23c88ccd02107bc0ef655
Gulshan <125683164+gulshan-pandey@users.noreply.github.com>
2024-05-25 03:34:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ document.getElementById('Game').addEventListener("keyup", (e) => {
     }
 
     // ------------ Move lines / words
-    if (currentWord.getBoundingClientRect().top > 400) {
+    if (currentWord.getBoundingClientRect().top > 450) {
         const words = document.getElementById('words');
         const margin = parseInt(words.style.marginTop || '0px');
         words.style.marginTop = margin - 25 + 'px';
@@ -290,7 +290,7 @@ document.getElementById('mobile-input-field').addEventListener('input', (e) => {
     }
 
     // ------------ Move lines / words
-    if (currentWord.getBoundingClientRect().top > 350) {
+    if (currentWord.getBoundingClientRect().top > 300) {
         const words = document.getElementById('words');
         const margin = parseInt(words.style.marginTop || '0px');
         words.style.marginTop = (margin - 35) + 'px';
